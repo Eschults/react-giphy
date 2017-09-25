23223e9c1f269ed571f65e0b18014fbed3495805
Edward Schults <edward.schults@gmail.com>
2017-09-25 21:07:45 +0200

    update title

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Gif = (props) => {
-  const url = `https://media1.giphy.com/media/${props.giphyId}/giphy.gif`
+  const url = `https://media1.giphy.com/media/${props.giphyId}/giphy.gif`;
   return <img src={url} alt="" className="gif" />;
 };
 
